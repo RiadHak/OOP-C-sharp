cd3e6451be624214c3f3b959a5b1a9e0bd1237f1
M.Riad Alhakim <89877825+ReiadHa@users.noreply.github.com>
2023-05-15 12:23:08 +0200

    opdracht 1

---
+++ opdracht 1/Program.cs
@@ -0,0 +1,41 @@
+﻿bool answer;
+do
+{
+    Console.WriteLine("wat is je Charmander's naam?");
+    string char_name = Console.ReadLine(); ;
+    Charmander ans = new Charmander(char_name, "Fire", "Water");
+    for (int i = 0; i < 10; i++)
+    {
+        ans.battle_cry();
+    }
+    Console.WriteLine("wil je dooe spelen?(Y/N)");
+    string inp = Console.ReadLine();
+    answer = inp == "Y" ? true : false;
+
+} while (answer);
+
+
+
+//Charmander pokemon = new Charmander("riad","fire","water");
+//pokemon.battle_cry();
+
+
+
+class Charmander
+{
+    public string name;
+    public string strength;
+    public string weakness;
+
+    public Charmander(string name, string strength, string weakness)
+    {
+        this.name = name;
+        this.strength = strength;
+        this.weakness = weakness;
+    }
+
+    public void battle_cry()
+    {
+        Console.WriteLine(this.name + " is zijn Battlecry aan het doen? ");
+    }
+}
