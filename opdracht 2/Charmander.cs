cc9c5ad96e826fc0b0de0790a1bd8742c1e47887
M.Riad Alhakim <89877825+ReiadHa@users.noreply.github.com>
2023-05-17 15:07:38 +0200

    opdracht 2 gemaakt

---
+++ opdracht 2/Charmander.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opdracht_2
+{
+    public class Charmander
+    {
+        public string name;
+        public string strength;
+        public string weakness;
+
+        public Charmander(string name, string strength, string weakness)
+        {
+            this.name = name;
+            this.strength = strength;
+            this.weakness = weakness;
+        }
+
+        public void battle_cry()
+        {
+                Console.WriteLine(this.name + " is zijn Battlecry aan het doen O<>O ");
+
+
+        }
+
+    }
+}
---
+++ opdracht 2/PokeBall.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opdracht_2
+{
+    public class PokeBall
+    {
+        private Charmander? _name;
+        public Charmander PokemoneName { get { return _name; } set { _name = value; } }
+
+
+
+        public PokeBall(Charmander name)
+        {
+            this._name = name;
+        }
+
+        public Charmander open_ball()
+        {
+            _name.battle_cry();
+            return _name;
+        }
+
+
+
+    }
+}
+
